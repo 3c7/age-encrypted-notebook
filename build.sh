9f856bb141fd32b47ccca704756b0230c19b7658
3c7 <3c7@posteo.de>
2022-02-14 22:41:00 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/usr/binb/env bash
 
-VERSION=$(git describe --tags)
+VERSION=$(git describe --tags --abbrev=0)
 build_aen() {
     if [ $GOOS == "windows" ]; then
-      go build -o "dist/aen-$GOOS.exe" -ldflags "-X main.Version=$VERSION" -trimpath ./cmd/aen.go
+      go build -o "dist/aen.exe" -ldflags "-X main.Version=$VERSION" -trimpath ./cmd/aen.go
+      cd dist && 7z a aen-$VERSION-$GOOS-$GOARCH.zip aen.exe && cd ..
+      rm dist/aen.exe
     else
-      go build -o "dist/aen-$GOOS" -ldflags "-X main.Version=$VERSION" -trimpath ./cmd/aen.go
+      go build -o "dist/aen" -ldflags "-X main.Version=$VERSION" -trimpath ./cmd/aen.go
+      cd dist && tar -cvzf aen-$VERSION-$GOOS-$GOARCH.tar.gz aen && cd ..
+      rm dist/aen
     fi
 }
 
 export CGO_ENABLED=0
-GOOS=windows build_aen
-GOOS=linux build_aen
-GOOS=darwin build_aen
+GOOS=windows GOARCH=amd64 build_aen
+GOOS=linux GOARCH=amd64 build_aen
+GOOS=darwin GOARCH=amd64 build_aen
