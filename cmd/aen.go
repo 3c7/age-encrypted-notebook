119ac350d06cd96a09b0c68bfdda8e4fa5610bec
3c7 <Nils.Kuhnert@posteo.de>
2022-02-28 22:45:01 +0100

    Typo

--- cmd/aen.go
+++ cmd/aen.go
@@ -44,7 +44,7 @@ More details via "aen help" or with parameter "--help".
 
 const help string = `Age Encrypted Notebook $(VERSION)
 
-* DB and keyfile paths can also be given via evironment variables AENDB and AENKEY.
+* DB and keyfile paths can also be given via environment variables AENDB and AENKEY.
 ** The default editor can be changed through setting the environment variable AENEDITOR.
 
 Usage:
